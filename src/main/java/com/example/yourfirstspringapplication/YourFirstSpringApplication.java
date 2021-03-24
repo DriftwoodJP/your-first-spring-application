bd86436a7a7d02a4683c0cd29c953395569ac111
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2021-03-24 14:59:17 +0900

    first project files

---
+++ src/main/java/com/example/yourfirstspringapplication/YourFirstSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourfirstspringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourFirstSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourFirstSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yourfirstspringapplication/YourFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yourfirstspringapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourFirstSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
